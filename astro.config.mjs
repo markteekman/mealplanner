48b011bdfcdae1790369e3f9dfd9e3e9876264d0
Mark Teekman <markteekman@users.noreply.github.com>
2023-02-12 09:08:14 +0100

    Add optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig ({
-  integrations: [svelte()]
+  integrations: [compress(), svelte()]
 })
