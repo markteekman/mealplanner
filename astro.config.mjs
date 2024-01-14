2995c1fa5c5f759845400894c86e28bf86fbd147
Mark Teekman <markteekman@users.noreply.github.com>
2024-01-14 10:17:58 +0100

    Remove Astro Compress npm package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import svelte from '@astrojs/svelte'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-export default defineConfig ({
-  integrations: [compress(), svelte()]
-})
+export default defineConfig({
+  integrations: [svelte()],
+});
